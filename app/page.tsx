4fbdb93cbfc985161a6253a772eb43300bcefbc6
RoJaebl <rojaebl@RoJaeblui-MacBookPro.local>
2024-08-06 18:23:38 +0900

    :hammer: #3.6 Form Modifiers: Peer

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,16 @@ export default function Home() {
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-rose-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5 ">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2">
         <input
-          className="w-full rounded-full h-10 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500"
+          className="w-full rounded-full h-10 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
           type="email"
           required
-          placeholder="Search here..."
+          placeholder="Email addresss"
         />
-        <button className="text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none md:px-10 bg-gradient-to-tr from-cyan-500 via-yellow-400 to-purple-400">
-          Search
+        <span className="text-red-500 font-medium hidden peer-invalid:block">
+          Email is required.
+        </span>
+        <button className="text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none md:px-10 bg-black peer-invalid:bg-red-100 peer-required:bg-green-500">
+          Log in
         </button>
       </div>
     </main>
