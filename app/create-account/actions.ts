a74e692ead7c68f47344236c573fbadc50c316d7
RoJaebl <rojaebl@RoJaeblui-MacBookPro.local>
2024-08-09 16:29:52 +0900

    :hammer: #6.2 Refinement

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,12 +1,33 @@
 "use server";
-import { z } from "zod";
+import { z, ZodString } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirmPassword: z.string().min(10),
-});
+const checkUsername = (username: string) => !username.includes("potato");
+const checkPassword = ({
+  password,
+  confirmPassword,
+}: {
+  password: string;
+  confirmPassword: string;
+}) => password === confirmPassword;
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "Username must be a string!",
+        required_error: "Where is my username???",
+      })
+      .min(3, "Way too short!!!")
+      .max(10, "That is too loooong!")
+      .refine(checkUsername, "No potatoes allowed!"),
+    email: z.string().email(),
+    password: z.string().min(10),
+    confirmPassword: z.string().min(10),
+  })
+  .refine(checkPassword, {
+    message: "Both passwords should be the same!",
+    path: ["confirmPassword"],
+  });
 
 export async function createAccount(prevState: unknown, formData: FormData) {
   const data = {
@@ -17,6 +38,7 @@ export async function createAccount(prevState: unknown, formData: FormData) {
   };
   const result = formSchema.safeParse(data);
   if (!result.success) {
+    console.log(result.error.flatten());
     return result.error.flatten();
   }
 }
