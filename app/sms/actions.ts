ae2498f4ed17c2fc1266e38afd61161322491ea0
RoJaebl <rojaebl@RoJaeblui-MacBookPro.local>
2024-08-09 20:10:44 +0900

    :hammer: #6.8 SMS Validation

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,13 +1,35 @@
 "use server";
 import { z } from "zod";
 import validator from "validator";
+import { redirect } from "next/navigation";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, "ko-KR"),
+    "Wrong phone format"
+  );
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
-export async function smsLogIn(prevState: any, formData: FormData) {
-  console.log(typeof formData.get("token"));
+export interface ActionState {
+  token: boolean;
+}
 
-  console.log(tokenSchema.safeParse(formData.get("token")));
-  return "";
+export async function smsLogIn(prevState: ActionState, formData: FormData) {
+  const phone = formData.get("phone");
+  const token = formData.get("token");
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+    console.log(result.error?.flatten());
+    return {
+      token: result.success,
+      ...(!result.success && { error: result.error.flatten() }),
+    };
+  } else {
+    const result = tokenSchema.safeParse(token);
+    return !result.success
+      ? { token: true, error: result.error.flatten() }
+      : redirect("/");
+  }
 }
