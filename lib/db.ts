6634da10ae5133084e63d437b0ada2efb8170ad2
RoJaebl <qgs0924@gmail.com>
2024-09-16 20:25:05 +0900

    :hammer: #7.4 Relationships

--- lib/db.ts
+++ lib/db.ts
@@ -3,20 +3,25 @@ import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 
 async function test() {
-  // const user = await db.user.create({
+  // const token = await db.sMSToken.create({
   //   data: {
-  //     username: "test",
+  //     token: "2314123",
+  //     user: {
+  //       connect: {
+  //         id: 2,
+  //       },
+  //     },
   //   },
   // });
-  // console.log(user);
-  const users = await db.user.findMany({
+  const token = await db.sMSToken.findUnique({
     where: {
-      username: {
-        contains: "est",
-      },
+      id: 1,
+    },
+    include: {
+      user: true,
     },
   });
-  console.log(users);
+  console.log(token);
 }
 
 test();
