e09e6bd4634ca42a45270d1bbbc3e88898e26eab
RoJaebl <qgs0924@gmail.com>
2024-09-17 21:38:24 +0900

    :hammer: #10.9 Infinite Scrolling

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { InitialProducts } from "@/app/(tabs)/products/page";
 import ListProduct from "./list-product";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { getMoreProducts } from "@/app/(tabs)/products/actions";
 
 interface ProductListProps {
@@ -12,33 +12,46 @@ export default function ProductList({ initialProducts }: ProductListProps) {
   const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [isLastPage, setIsLastPage] = useState(false);
-  const onLoadMoreClick = async () => {
-    setIsLoading(true);
-    const newProducts = await getMoreProducts(page);
-    if (newProducts.length !== 0) {
-      setPage((prev) => ++prev);
-      setProducts((prev) => [...prev, ...newProducts]);
-    } else {
-      setIsLastPage(true);
+  const trigger = useRef<HTMLSpanElement>(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      async (entries) => {
+        const element = entries[0];
+        if (element.isIntersecting && trigger.current) {
+          observer.unobserve(trigger.current);
+          setIsLoading(true);
+          const newProducts = await getMoreProducts(page);
+          if (newProducts.length !== 0) {
+            setPage((prev) => ++prev);
+            setProducts((prev) => [...prev, ...newProducts]);
+          } else {
+            setIsLastPage(true);
+          }
+          setIsLoading(false);
+        }
+      },
+      { threshold: 1.0, rootMargin: "0px 0px -100px 0px" },
+    );
+    if (trigger.current) {
+      observer.observe(trigger.current);
     }
-    setIsLoading(false);
-  };
+    return () => observer.disconnect();
+  }, [page]);
+
   return (
     <div className="flex flex-col gap-5 p-5">
       {products.map((product) => (
         <ListProduct key={product.id} {...product} />
       ))}
-      {isLastPage ? (
-        "No more items"
-      ) : (
-        <button
-          onClick={onLoadMoreClick}
-          disabled={isLoading}
-          className="mx-auto w-fit rounded-md bg-orange-500 px-3 py-2 text-sm font-semibold duration-300 hover:opacity-90 active:scale-95"
+      {!isLastPage ? (
+        <span
+          ref={trigger}
+          style={{ marginTop: `${page * 900}vh` }}
+          className="mx-auto mb-96 w-fit rounded-md bg-orange-500 px-3 py-2 text-sm font-semibold duration-300 hover:opacity-90 active:scale-90"
         >
           {isLoading ? "로딩 중" : "Load more"}
-        </button>
-      )}
+        </span>
+      ) : null}
     </div>
   );
 }
--- components/tab-bar.tsx
+++ components/tab-bar.tsx
@@ -20,44 +20,44 @@ import { usePathname } from "next/navigation";
 export default function TabBar() {
   const pathname = usePathname();
   return (
-    <div className="fixed bottom-0 w-full mx-auto max-w-screen-md grid grid-cols-5 border-neutral-600 border-t px-5 py-3 *:text-white">
+    <div className="fixed bottom-0 mx-auto grid w-full max-w-screen-md grid-cols-5 border-t border-neutral-600 bg-neutral-800 px-5 py-3 *:text-white">
       <Link href="/products" className="flex flex-col items-center gap-px">
         {pathname === "/products" ? (
-          <SolidHomeIcon className="w-7 h-7" />
+          <SolidHomeIcon className="h-7 w-7" />
         ) : (
-          <OutlineHomeIcon className="w-7 h-7" />
+          <OutlineHomeIcon className="h-7 w-7" />
         )}
         <span>홈</span>
       </Link>
       <Link href="/life" className="flex flex-col items-center gap-px">
         {pathname === "/life" ? (
-          <SolidNewspaperIcon className="w-7 h-7" />
+          <SolidNewspaperIcon className="h-7 w-7" />
         ) : (
-          <OutlineNewspaperIcon className="w-7 h-7" />
+          <OutlineNewspaperIcon className="h-7 w-7" />
         )}
         <span>동네생활</span>
       </Link>
       <Link href="/chat" className="flex flex-col items-center gap-px">
         {pathname === "/chat" ? (
-          <SolidChatIcon className="w-7 h-7" />
+          <SolidChatIcon className="h-7 w-7" />
         ) : (
-          <OutlineChatIcon className="w-7 h-7" />
+          <OutlineChatIcon className="h-7 w-7" />
         )}
         <span>채팅</span>
       </Link>
       <Link href="/live" className="flex flex-col items-center gap-px">
         {pathname === "/live" ? (
-          <SolidVideoCameraIcon className="w-7 h-7" />
+          <SolidVideoCameraIcon className="h-7 w-7" />
         ) : (
-          <OutlineVideoCameraIcon className="w-7 h-7" />
+          <OutlineVideoCameraIcon className="h-7 w-7" />
         )}
         <span>쇼핑</span>
       </Link>
       <Link href="/profile" className="flex flex-col items-center gap-px">
         {pathname === "/profile" ? (
-          <SolidUserIcon className="w-7 h-7" />
+          <SolidUserIcon className="h-7 w-7" />
         ) : (
-          <OutlineUserIcon className="w-7 h-7" />
+          <OutlineUserIcon className="h-7 w-7" />
         )}
         <span>나의 당근</span>
       </Link>
