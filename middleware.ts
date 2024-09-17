b248796e810e62ffcd6d26083abb9ee300b63156
RoJaebl <qgs0924@gmail.com>
2024-09-17 14:34:02 +0900

    :hammer: #8.10 Matcher

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,19 @@
-import { NextRequest } from "next/server";
+import { MiddlewareConfig, NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  console.log(session);
-  if (request.nextUrl.pathname === "/profile") {
-    return Response.redirect(new URL("/", request.url));
-  }
+  console.log("hello");
+  // const pathname = request.nextUrl.pathname;
+  // if (pathname === "/") {
+  //   const response = NextResponse.next();
+  //   response.cookies.set("middleware-cookie", "hello!");
+  //   return response;
+  // }
+  // if (request.nextUrl.pathname === "/profile") {
+  //   return Response.redirect(new URL("/", request.url));
+  // }
 }
+
+export const config: MiddlewareConfig = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
